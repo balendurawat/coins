8cb031bfaa1c7b37a1df8010122cff06fd9993d7
Balendu Rawat <balendurawat1@gmail.com>
2022-10-09 13:55:43 +0530

    smart contracts

--- smart_contracts/procoin_ico.sol
+++ smart_contracts/procoin_ico.sol
@@ -9,4 +9,24 @@ contract procoin_ico {
 
     // procoins bought already
     uint public total_procoins_bought = 0;
+
+
+    mapping(address => uint) equity_procoin;
+    mapping(address => uint) equity_usd;
+
+    modifier can_buy_procoin(uint usd_invested) {
+        require(usd_invested * usd_to_procoins + total_procoins_bought <= max_procoins);
+    }
+
+    function equity_in_procoins(address investor) external constant returns(uint){
+        return equity_procoin[investor];
+    }
+
+
+    function equity_in_usd(address investor) external constant returns(uint){
+        return equity_usd[investor];
+    }
+
+
+
 }
